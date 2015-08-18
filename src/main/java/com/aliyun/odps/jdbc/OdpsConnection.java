e7a7d3e8dd425bcfc1f4b695e88cd8d906d2054b
程亦超(何兮) <yichao.cheng@alibaba-inc.com>
2015-08-18 15:35:41 +0800

    use LogView in 0.18.3

--- src/main/java/com/aliyun/odps/jdbc/OdpsConnection.java
+++ src/main/java/com/aliyun/odps/jdbc/OdpsConnection.java
@@ -48,13 +48,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.aliyun.odps.Instance;
+import com.aliyun.odps.LogView;
 import com.aliyun.odps.Odps;
 import com.aliyun.odps.OdpsException;
 import com.aliyun.odps.account.Account;
 import com.aliyun.odps.account.AliyunAccount;
 import com.aliyun.odps.task.SQLTask;
-import com.aliyun.odps.utils.StringUtils;
-import com.aliyun.odps.security.SecurityManager;
 
 public class OdpsConnection extends WrapperAdapter implements Connection {
 
@@ -456,56 +455,11 @@ public class OdpsConnection extends WrapperAdapter implements Connection {
     return this.odps.getEndpoint();
   }
 
-  public class LogView {
-
-    private static final String POLICY_TYPE = "BEARER";
-    private static final String HOST_DEFAULT = "http://webconsole.odps.aliyun-inc.com:8080";
-    private String logViewHost = HOST_DEFAULT;
-
-    Odps odps;
-
-    public LogView(Odps odps) {
-      this.odps = odps;
-      if (odps.getLogViewHost() != null) {
-        logViewHost = odps.getLogViewHost();
-      }
-    }
-
-    public String generateLogView(Instance instance, long hours) throws OdpsException {
-      if (StringUtils.isNullOrEmpty(logViewHost)) {
-        return "";
-      }
-
-      SecurityManager sm = odps.projects().get(instance.getProject()).getSecurityManager();
-      String policy = generatePolicy(instance, hours);
-      String token = sm.generateAuthorizationToken(policy, POLICY_TYPE);
-      String logview = logViewHost + "/logview/?h=" + odps.getEndpoint() + "&p="
-                       + instance.getProject() + "&i=" + instance.getId() + "&token=" + token;
-      return logview;
-    }
-
-    private String generatePolicy(Instance instance, long hours) {
-      String policy = "{\n" //
-                      + "    \"expires_in_hours\": " + String.valueOf(hours) + ",\n" //
-                      + "    \"policy\": {\n" + "        \"Statement\": [{\n"
-                      + "            \"Action\": [\"odps:Read\"],\n"
-                      + "            \"Effect\": \"Allow\",\n" //
-                      + "            \"Resource\": \"acs:odps:*:projects/" + instance.getProject()
-                      + "/instances/"
-                      + instance.getId() + "\"\n" //
-                      + "        }],\n"//
-                      + "        \"Version\": \"1\"\n" //
-                      + "    }\n" //
-                      + "}";
-      return policy;
-    }
-  }
-
   /**
    * Kick-offer
    *
    * @param sql sql string
-   * @return an intance
+   * @return an instance
    * @throws SQLException
    */
   protected Instance runClientSQL(String sql) throws SQLException {
