768e60fd16d5bda18dd1a0be7585905cb97a76c4
unknown <ximo.zy@alibaba-inc.com>
2015-09-14 15:32:11 +0800

    add test cases

--- src/test/java/com/aliyun/odps/jdbc/qa/TestBase.java
+++ src/test/java/com/aliyun/odps/jdbc/qa/TestBase.java
@@ -1,6 +1,8 @@
 package com.aliyun.odps.jdbc.qa;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.InputStream;
@@ -25,6 +27,7 @@ public class TestBase {
             try {
                 conn = testBase.getConnection();
                 statement = conn.createStatement();
+                setUp();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -60,6 +63,44 @@ public class TestBase {
         Assert.assertTrue(testBase != null && conn != null);
     }
 
+    public static void setUp() throws Exception {
+        statement.executeUpdate(
+                "drop table if exists odps_jdbc_t10000;");
+        statement.executeUpdate(
+                "create table if not exists odps_jdbc_t10000(id bigint,name string);");
+        statement.executeUpdate(
+                "insert into table odps_jdbc_t10000 select count(*),count(*) from odps_jdbc_t10000;");
+        statement.executeUpdate(
+                "insert into table odps_jdbc_t10000 select count(*),count(*) from odps_jdbc_t10000;");
+        statement.executeUpdate(
+                "insert into table odps_jdbc_t10000 select count(*),count(*) from odps_jdbc_t10000;");
+    }
 
+
+    public static void tearDown() throws Exception {
+        statement.executeUpdate(
+                "drop table if exists odps_jdbc_t10000");
+        statement.close();
+        conn.close();
+    }
+
+    // Print resultSet
+    public void printRs(ResultSet resultSet) throws Exception {
+        ResultSetMetaData meta = resultSet.getMetaData();
+        for (int i = 1; i <= meta.getColumnCount(); i++) {
+            System.out.print("\t" + meta.getColumnName(i));
+        }
+        for (int i = 1; i <= meta.getColumnCount(); i++) {
+            System.out.print("\t" + meta.getColumnTypeName(i));
+        }
+        System.out.println();
+
+        while (resultSet.next()) {
+            for (int i = 1; i <= meta.getColumnCount(); i++) {
+                System.out.print("\t" + resultSet.getObject(i));
+            }
+            System.out.println();
+        }
+    }
 }
 
