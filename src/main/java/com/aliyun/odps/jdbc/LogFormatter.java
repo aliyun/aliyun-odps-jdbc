c2a93f4c4774830c33e858dad8eece19602f690a
程亦超(何兮) <yichao.cheng@alibaba-inc.com>
2015-11-04 09:58:11 +0800

    add an one-line log formatter

---
+++ src/main/java/com/aliyun/odps/jdbc/LogFormatter.java
@@ -0,0 +1,14 @@
+package com.aliyun.odps.jdbc;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class LogFormatter extends Formatter {
+
+  @Override
+  public String format(LogRecord record) {
+    return "[" + record.getLevel() + "] " + record.getMessage() + "\n";
+  }
+}
+
+
