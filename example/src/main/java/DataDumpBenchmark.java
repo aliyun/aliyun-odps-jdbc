a4df0d1fbdf932992fb79b615561026a2c95afd3
程亦超(何兮) <yichao.cheng@alibaba-inc.com>
2015-11-27 16:40:45 +0800

    use multithreads in the DataDump example

--- example/src/main/java/DataDumpBenchmark.java
+++ example/src/main/java/DataDumpBenchmark.java
@@ -1,98 +1,212 @@
-import java.io.IOException;
 import java.io.InputStream;
 import java.lang.System;
-import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.DriverManager;
-import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class DataDumpBenchmark {
+
   private static String driverName = "com.aliyun.odps.jdbc.OdpsDriver";
+
+
+  static String joinStrings(String[] s, String glue) {
+    int k = s.length;
+    if (k == 0) {
+      return null;
+    }
+    StringBuilder out = new StringBuilder();
+    out.append(s[0]);
+    for (int x = 1; x < k; ++x) {
+      out.append(glue).append(s[x]);
+    }
+    return out.toString();
+  }
 
   /**
    * @param args
    * @throws SQLException
    */
   public static void main(String[] args) throws SQLException {
+
+    final int batchSize;
+    final int splitNum;
+    final Properties odpsConfig = new Properties();
     try {
+      splitNum = Integer.parseInt(args[0]);
+      batchSize = Integer.parseInt(args[1]);
+      System.out.println("split size: " + batchSize);
+      System.out.println("batch size: " + splitNum);
+
       Class.forName(driverName);
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
 
-    int batchSize;
+      InputStream is =
+          Thread.currentThread().getContextClassLoader().getResourceAsStream("conf.properties");
+      odpsConfig.load(is);
 
-    try {
-      batchSize = Integer.parseInt(args[0]);
     } catch (Exception e) {
       System.out.println(e);
-      batchSize = 50000;
+      return;
     }
-    System.out.println("batch size: " + batchSize);
 
-    // fill in the information string
-    Properties odpsConfig = new Properties();
-    InputStream is =
-        Thread.currentThread().getContextClassLoader().getResourceAsStream("conf.properties");
-    try {
-      odpsConfig.load(is);
-    } catch (IOException e) {
-      e.printStackTrace();
-      System.exit(1);
+    final Connection connSource = DriverManager.getConnection(
+        "jdbc:odps:" + odpsConfig.getProperty("endpoint_1") + "?project=meta_dev&loglevel=debug&",
+        odpsConfig.getProperty("access_id_1"),
+        odpsConfig.getProperty("access_key_1"));
+
+    final Connection connTarget = DriverManager.getConnection(
+        "jdbc:odps:" + odpsConfig.getProperty("endpoint_2") + "?project=odps_test_sqltask_finance&loglevel=debug",
+        odpsConfig.getProperty("access_id_2"),
+        odpsConfig.getProperty("access_key_2"));
+
+    String sourceSchema = "meta";
+    String sourceTable = "m_instance";
+    String targetTable = "m_instance_copy";
+    String sourceWhere = "ds = '20151120'";
+    String splitColumn = "start_time";
+
+    ResultSet cols = connSource.getMetaData().getColumns(null, sourceSchema, sourceTable, null);
+    List<String> nameTypePairs = new ArrayList<String>();
+    List<String> questionMarks = new ArrayList<String>();
+    while (cols.next()) {
+      nameTypePairs.add(cols.getString("COLUMN_NAME") + " " + cols.getString("TYPE_NAME"));
+      questionMarks.add("?");
     }
-    String accessId = odpsConfig.getProperty("access_id");
-    String accessKey = odpsConfig.getProperty("access_key");
-    Connection conn = DriverManager.getConnection(odpsConfig.getProperty("connection_string"), accessId, accessKey);
 
-    Statement ddl = conn.createStatement();
-    ddl.executeUpdate("drop table if exists m_instance_one_day;");
-    ddl.executeUpdate(
-        "create table m_instance_one_day(c1 string, c2 string, c3 string, c4 string, c5 string, c6 string, c7 bigint, c8 bigint, c9 bigint, c10 string);");
+    final int colNums = nameTypePairs.size();
+    if (sourceSchema != null) {
+      sourceTable = sourceSchema + "." + sourceTable;
+    }
 
-    PreparedStatement ps = conn.prepareStatement(
-        "insert into m_instance_one_day values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
-    Statement query =  conn.createStatement();
-    ResultSet rs = query.executeQuery("select * from meta.m_instance where ds = '20151120' limit 1000000;");
+    String createTableSql = "create table " + targetTable + "(" +
+                            joinStrings(nameTypePairs.toArray(new String[nameTypePairs.size()]),
+                                        ", ") + ")";
+    final String insertValuesSql = "insert into " + targetTable + " values (" +
+                             joinStrings(questionMarks.toArray(new String[questionMarks.size()]),
+                                         ", ") + ")";
+
+    String rangeSql = "select max(" + splitColumn + "), min(" + splitColumn + ") from " + sourceTable;
+    if (sourceWhere != null) {
+      rangeSql += " where " + sourceWhere;
+    }
+
+    Statement ddl = connTarget.createStatement();
+    ddl.executeUpdate("drop table if exists " + targetTable);
+    ddl.executeUpdate(createTableSql);
+    ddl.close();
+
+    Statement query = connSource.createStatement();
+    ResultSet range = query.executeQuery(rangeSql);
+    long max, min;
+    range.next();
+    max = range.getLong(1);
+    min = range.getLong(2);
+    range.close();
+    query.close();
+
+    long[] markers = new long[splitNum + 1];
+    markers[0] = min;
+    for (int i = 1; i < splitNum; i++) {
+      markers[i] = markers[i - 1] + (max - min + 1) / splitNum;
+    }
+    markers[splitNum] = max;
+
+    String[] cutters = new String[splitNum];
+    for (int i = 0; i < splitNum - 1; i++) {
+      cutters[i] = markers[i] + " <= " + splitColumn + " and " + splitColumn + " < " + markers[i + 1];
+    }
+    cutters[splitNum - 1] =
+        markers[splitNum - 1] + " <= " + splitColumn + " and " + splitColumn + " <= " + markers[splitNum];
+
+    connSource.close();
+    connTarget.close();
+
 
     long start = System.currentTimeMillis();
-    long now = start;
 
-    int count = 0;
-    while (rs.next()) {
-      ps.setString(1, rs.getString(1));
-      ps.setString(2, rs.getString(2));
-      ps.setString(3, rs.getString(3));
-      ps.setString(4, rs.getString(4));
-      ps.setString(5, rs.getString(5));
-      ps.setString(6, rs.getString(6));
-      ps.setLong(7, rs.getLong(7));
-      ps.setLong(8, rs.getLong(8));
-      ps.setLong(9, rs.getLong(9));
-      ps.setString(10, rs.getString(10));
 
-      ps.addBatch();
-      if(++count % batchSize == 0) {
-        ps.executeBatch();
-        long end = System.currentTimeMillis();
-        System.out.printf("batch time: %.2f seconds\n", (float) (end - now) / 1000);
-        now = end;
+    ArrayList<Callable<Long>> callList = new ArrayList<Callable<Long>>();
+    for (int i = 0; i < splitNum; i++) {
+
+      String selectSql = "select * from " + sourceTable + " where ";
+      selectSql += cutters[i];
+      if (sourceWhere != null) {
+        selectSql += " and " + sourceWhere;
       }
+
+      final String selectSql2 = selectSql;
+      Callable<Long> call = new Callable<Long>() {
+        @Override
+        public Long call() throws Exception {
+          final Connection connSource = DriverManager.getConnection(
+              "jdbc:odps:" + odpsConfig.getProperty("endpoint_1")
+              + "?project=meta_dev&loglevel=debug&",
+              odpsConfig.getProperty("access_id_1"),
+              odpsConfig.getProperty("access_key_1"));
+
+          final Connection connTarget = DriverManager.getConnection(
+              "jdbc:odps:" + odpsConfig.getProperty("endpoint_2")
+              + "?project=odps_test_sqltask_finance&loglevel=debug",
+              odpsConfig.getProperty("access_id_2"),
+              odpsConfig.getProperty("access_key_2"));
+
+          Statement query = connSource.createStatement();
+          PreparedStatement insert = connTarget.prepareStatement(insertValuesSql);
+          ResultSet rs = query.executeQuery(selectSql2);
+          long start = System.currentTimeMillis();
+          long now = start;
+
+          int count = 0;
+          while (rs.next()) {
+            for (int i = 0; i < colNums; i++) {
+              try {
+                insert.setObject((i + 1), rs.getObject(i + 1));
+              } catch (SQLException e) {
+                e.printStackTrace();
+                System.exit(1);
+              }
+            }
+
+            insert.addBatch();
+            if (++count % batchSize == 0) {
+              insert.executeBatch();
+              long end = System.currentTimeMillis();
+              System.out.printf("batch time: %.2f seconds\n", (float) (end - now) / 1000);
+              now = end;
+            }
+          }
+
+          insert.executeBatch(); // insert remaining records
+          rs.close();
+          query.close();
+          insert.close();
+          connSource.close();
+          connTarget.close();
+          return 0L;
+        }
+      };
+      callList.add(call);
     }
 
-    ps.executeBatch(); // insert remaining records
+    ExecutorService executors = Executors.newFixedThreadPool(2);
+    try {
+      executors.invokeAll(callList);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+      return;
+    }
+    executors.shutdown();
+
     System.out.printf("total: %.2f minutes\n",
                       (float) (System.currentTimeMillis() - start) / 1000 / 60);
-
-    ddl.close();
-    ps.close();
-    rs.close();
-    query.close();
-    conn.close();
   }
 }
