b38b22bdb383a9c81ef87660c5895d0a6c729638
jon <wangzhong.zw@alibaba-inc.com>
2019-05-28 12:17:28 +0800

    # Please remove the below "#" ahead of kelude bug info, the first one will be the git commit subject
    Add odps logger
    
    Change-Id: I56643074a5178191fef2aad17c54a2a8bbe96c65

---
+++ src/main/java/com/aliyun/odps/jdbc/utils/OdpsFormatter.java
@@ -0,0 +1,15 @@
+package com.aliyun.odps.jdbc.utils;
+
+import java.sql.Timestamp;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class OdpsFormatter extends Formatter {
+
+  @Override
+  public synchronized String format(LogRecord record) {
+    Timestamp timestamp = new Timestamp(record.getMillis());
+    String format = "[%s] [%s] %s\n";
+    return String.format(format, record.getLevel(), timestamp.toString(), record.getMessage());
+  }
+}
