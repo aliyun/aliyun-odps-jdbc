8fd771bc860e1573d487901b4655928548ff6afb
dongxiao.dx <dongxiao.dx@alibaba-inc.com>
2020-02-26 17:16:40 +0800

    [to #25329541][SESSION] update jdbc version and throw exception when IOException

--- src/main/java/com/aliyun/odps/jdbc/OdpsForwardResultSet.java
+++ src/main/java/com/aliyun/odps/jdbc/OdpsForwardResultSet.java
@@ -155,7 +155,9 @@ public class OdpsForwardResultSet extends OdpsResultSet implements ResultSet {
         }
 
         fetchedRows++;
-
+        conn.log.info("Fetch record, current fetchedRows:"
+            + fetchedRows + ",reuseRecord:" + reuseRecord
+            + ",reader:" + reader + ",reader.getTotalBytes():" + reader.getTotalBytes());
         // Log the time consumption for fetching a bunch of rows
         if (fetchedRows % ACCUM_FETCHED_ROWS == 0 && fetchedRows != 0) {
           long delta = reader.getTotalBytes() / 1024 - accumKBytes;
@@ -175,8 +177,9 @@ public class OdpsForwardResultSet extends OdpsResultSet implements ResultSet {
         // Can not rebuild tunnel reader in session mode, just retry reading
         if (!sessionMode) {
           rebuildReader();
+        } else {
+          throw new SQLException("Read result failed: " + e.getMessage());
         }
-
       }
     }
   }
