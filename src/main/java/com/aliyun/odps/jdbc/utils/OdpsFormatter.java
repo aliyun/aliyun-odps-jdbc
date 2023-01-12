7460d175fa85f418e2b9cf9e3f61926f65e1a06c
述作 <jiangyefan.jyf@alibaba-inc.com>
2023-01-12 11:15:28 +0800

    [to #47240695] fix print log duplicate error and connection id increase not work.

--- src/main/java/com/aliyun/odps/jdbc/utils/OdpsFormatter.java
+++ src/main/java/com/aliyun/odps/jdbc/utils/OdpsFormatter.java
@@ -5,27 +5,19 @@ import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class OdpsFormatter extends Formatter {
-  private String connectionId = null;
 
   OdpsFormatter() {
     super();
   }
 
-  OdpsFormatter(String connectionId) {
-    super();
-
-    this.connectionId = connectionId;
-  }
-
   @Override
   public synchronized String format(LogRecord record) {
     Timestamp timestamp = new Timestamp(record.getMillis());
-    String format = "[%s] [%s] [connection-%s] [%s] %s\n";
+    String format = "[%s] [%s] [%s] %s\n";
     return String.format(
         format,
         record.getLevel(),
         record.getLoggerName(),
-        this.connectionId,
         timestamp.toString(),
         record.getMessage());
   }
